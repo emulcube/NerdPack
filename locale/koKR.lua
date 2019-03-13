638b09a88b60818b80dcc6f8a6e9d5318652ba34
emulcube <emulcube@gmail.com>
2019-03-13 21:00:01 +0900

    Update koKR.lua

--- locale/koKR.lua
+++ locale/koKR.lua
@@ -1,3 +1,22 @@
---local n_name, NeP = ...
+local n_name, NeP = ...
 
---NeP.Locale.koKR = {}
+NeP.Locale.koKR = {
+	Any = {
+  NeP_Show = '표시하기 '..n_name..' Execute: \n/Eep 보이기',
+  ON = '켜짐',
+  OFF = '꺼짐'
+	},
+	메인 창 = {
+	MasterToggle = '왼쪽 클릭: 활성화/비활성화\n오른쪽 클릭: 설정 열기',
+	Interrupts = '차단 켜기 or 끄기',
+	Cooldowns = '쿨 다운 켜기 or 끄기',
+	AoE = '광역스킬 켜기 or 끄기',
+	Settings = '설정',
+	HideNeP = '숨기기 '..n_name,
+	ChangeCR = '변경된 전투 로그:',
+	Donate = '기부자 '..n_name,
+	Forum = '포럼에 방문',
+	CRS = '전투 순환',
+	CRS_ST = '전투 순환 설정'
+	},
+}
